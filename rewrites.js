40cc29d52c6c14acdd95432c20fd2f119c5e4151
Leonardo Ortiz <leo@monogram.io>
2023-11-28 20:04:12 -0300

    _app rewrites

--- rewrites.js
+++ rewrites.js
@@ -2,5 +2,9 @@ module.exports = [
 	{
 		source: '/monogram-about',
 		destination: 'https://monogram.io/about'
+	},
+	{
+		source: '/_app/:path*',
+		destination: 'https://monogram.io/_app/:path*'
 	}
 ]
